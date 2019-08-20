e282f2eb3ac202e11baf6fa3aa9020b00dcf2cfb
renanparis <paris.oliveira@gmail.com>
2019-08-20 17:36:42 -0300

    Início do projeto.

---
+++ app/src/main/java/com/renanparis/finances/ui/activity/ListTransactionsActivity.kt
@@ -0,0 +1,13 @@
+package com.renanparis.finances.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.renanparis.finances.R
+
+class ListTransactionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+}
