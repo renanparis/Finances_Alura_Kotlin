1eb97233ed2d9995dcad5d55858621dfdd7b73ea
renanparis <paris.oliveira@gmail.com>
2019-09-05 17:04:34 -0300

    Configuração do AlertDialog para adição de receita.

--- app/src/main/java/com/renanparis/finances/ui/activity/ListTransactionsActivity.kt
+++ app/src/main/java/com/renanparis/finances/ui/activity/ListTransactionsActivity.kt
@@ -1,18 +1,24 @@
 package com.renanparis.finances.ui.activity
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.renanparis.finances.R
+import com.renanparis.finances.extensions.formatToBR
 import com.renanparis.finances.model.Transaction
 import com.renanparis.finances.model.Type
 import com.renanparis.finances.ui.ViewResume
 import com.renanparis.finances.ui.adapter.ListTransactionAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import kotlinx.android.synthetic.main.form_transacao.view.*
 import java.math.BigDecimal
+import java.util.*
 
 class ListTransactionsActivity : AppCompatActivity() {
 
@@ -29,10 +35,35 @@ class ListTransactionsActivity : AppCompatActivity() {
             val viewCreated =
                 LayoutInflater.from(this)
                     .inflate(R.layout.form_transacao, view as ViewGroup, false)
+          val today =   Calendar.getInstance()
+
+            viewCreated.form_transacao_data.setText(today.formatToBR())
+
+            val year = 2019
+            val month = 8
+            val day = 5
+
+            viewCreated.form_transacao_data.setOnClickListener {DatePickerDialog(this,
+                DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+
+                    val dateSelected = Calendar.getInstance()
+
+                    dateSelected.set(year,  month, dayOfMonth)
+
+                    viewCreated.form_transacao_data.setText(dateSelected.formatToBR())
+
+                }, year, month, day).show()}
+
+            val adapter = ArrayAdapter.createFromResource(this,
+                R.array.categorias_de_receita, android.R.layout.simple_spinner_dropdown_item)
+
+            viewCreated.form_transacao_categoria.adapter = adapter
 
             AlertDialog.Builder(this)
                 .setTitle(R.string.adiciona_receita)
                 .setView(viewCreated)
+                .setPositiveButton("Adicionar", null)
+                .setNegativeButton("Cancelar", null)
                 .show()
         }
 
